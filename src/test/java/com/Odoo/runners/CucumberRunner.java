9fc031fb0da1542c240dd6dc32cc4dcd0990692a
EdaYamanoglu <55071171+EdaYamanoglu@users.noreply.github.com>
2024-11-18 14:33:14 -0600

    Update CucumberRunner.java

--- src/test/java/com/Odoo/runners/CucumberRunner.java
+++ src/test/java/com/Odoo/runners/CucumberRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue="com/Odoo/step_definitions",
         dryRun = false,
-        tags="@Odoo_Eda"
+        tags="  @Odoo_Tst"
 
 )
 public class CucumberRunner {
